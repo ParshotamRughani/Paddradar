e8fbfce134dbe1437f3255dcff13e4cb050677e9
Misbah <56439527+hofferzz101@users.noreply.github.com>
2021-10-21 09:33:42 +0500

    headerfooter

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PrimarySearchAppBar from './dappradarComp/header/Header'
+import Footer from './dappradarComp/footer/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <PrimarySearchAppBar/>
+      <Footer/>
+    </div>
+    
+  );
+}
+
+export default App;
+
+
+
